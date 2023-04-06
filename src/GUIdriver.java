d763f336164ba6638811d279a96ea922f94d67a5
Ajax <thelegohexbug@gmail.com>
2023-04-06 18:35:03 -0400

    tagged author for javadocs

--- src/GUIdriver.java
+++ src/GUIdriver.java
@@ -8,6 +8,12 @@ import javafx.stage.Stage;
  */
 public class GUIdriver extends Application{
     
+    /**
+     * Runs our GUI, with the ability to swap from one page to another.
+     * 
+     * @author William Carr
+     * @param args Command line input (ignore)
+     */
     public static void main(String[] args) {
         launch(args);
     }
@@ -15,15 +21,16 @@ public class GUIdriver extends Application{
     /**
      * A start method that allows the pages to swap from one screen to another
      * 
+     * @author William Carr
      * @param mainStage The stage of the application that we will change.
      */
     @Override
     public void start(Stage mainStage) {
         HomeFrame home = new HomeFrame(mainStage);
         
-        // listener for login //TODO ask myers if this is bad design
+        // listener for login 
         home.getLoginButton().setOnAction(e -> Login_Frame.buildLoginPage(mainStage));
-        Login_Frame.getLoginButton().setOnAction(e -> mainStage.setScene(home.getScene())); //TODO change this because i just used the login button since there was no back button
+        Login_Frame.getLoginButton().setOnAction(e -> mainStage.setScene(home.getScene())); //TODO change this to back button when it's added
     }
 
 }
