89f179e9e1d2dc02b7184f386a3941a4d13ad3bd
Ajax <thelegohexbug@gmail.com>
2023-06-20 17:42:45 -0400

    added a hashmap of filters to use when searching

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,14 +1,25 @@
 import java.util.function.BiPredicate;
 import java.util.List;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 /**
  * A searching class for searching a list of providers given a criteria.
  * 
- * @author Julius A.Leone
+ * @author Julius A.Leone, William Carr
  * @version 4.16.2023
  */
-public class Searcher { //TODO let's put in some default filters for the search so that the search screen isnt empty at first
+public class Searcher {
+
+    private static HashMap<String, Searcher> defaultFilters = new HashMap<String,Searcher>(); // holds the default filters
+
+    static { //TODO maybe add filters called "has" and "lacks" to check if they have any items in inventory with keyword
+        //filter for name that contains a keyword
+        defaultFilters.put("contains",new Searcher((provider, string) -> provider.getName().toLowerCase().contains(string.toLowerCase())));
+        //filter for name that does not have a keyword
+        defaultFilters.put("without",new Searcher((provider, string) -> !provider.getName().toLowerCase().contains(string.toLowerCase())));
+    }
+
     private BiPredicate<Provider, String> filter;
 
     /**
@@ -36,12 +47,21 @@ public class Searcher { //TODO let's put in some default filters for the search 
         this.filter = filter;
     }
 
+    /**
+     * Returns a default Searcher
+     * @param filter The name of the default Searcher(i.e. "contains")
+     * @return A default method of searching
+     * @author William Carr
+     */
+    public static Searcher getDefault(String filter) {
+        return defaultFilters.get(filter);
+    }
 
     /**
      * Searches a list of providers and returns a list only including providers that match search parameters.
      * @param providers a list containing providers
      * @param searchString the string we are looking for
-     * @return
+     * @return The list of providers that fit the Searcher's filter
      */
     public List<Provider> searchProviders(List<Provider> providers, String searchString){
         return providers.parallelStream()
--- src/TestingDriver.java
+++ src/TestingDriver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Acts as a driver for tests on the program
@@ -9,19 +10,24 @@ import java.util.ArrayList;
 public class TestingDriver {
     
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         // UserBase.foodPantrys.put("anotheruser", new FoodPantry("anotheruser", "used", 0.0, 0.0, "Testing FoodPantry", "test"));
         
         // for(int i=0; i < 100; i++) System.out.println("\n");
 
         // UserBase.serializeFoodPantrys();
         
-        Searcher defSearcher = new Searcher((provider, string) -> provider.getName().toLowerCase().contains(string.toLowerCase()));
+        // Searcher defSearcher = new Searcher((provider, string) -> provider.getName().toLowerCase().contains(string.toLowerCase()));
 
-        System.out.println(defSearcher.searchProviders(UserBase.providers, "e"));//TODO this is how we want to do it generally, but could store a second Searcher method of doing it where it doesn't contain the phrase
+        // System.out.println(defSearcher.searchProviders(UserBase.providers, "e"));
         // UserBase.providers.get(0).setName("Fusha");
         // UserBase.serializeFoodPantrys();
         // System.out.println(UserBase.providers);
 
+        String s = sc.nextLine();
+        System.out.println(Searcher.getDefault("without").searchProviders(UserBase.providers, s));
         
     }
 
