1569c5f12f142b42dda7356ba5dbc971f26fcef5
Ajax <thelegohexbug@gmail.com>
2023-07-02 19:13:16 -0400

    added a helper method to get the names of types

--- src/ItemType.java
+++ src/ItemType.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Class for the different types of products that we can have
  * @author Alexa Gonzalez
@@ -6,4 +8,18 @@ public enum ItemType {
     FOOD,
     WATER,
     HYGIENE_PRODUCTS;
+
+    /**
+     * A list to return an app-usable list of the names of itemtypes.
+     * Must be updated with the addition of new ItemTypes.
+     * @return A list of all the ItemTypes
+     * @author William Carr
+     */
+    public static ArrayList<String> getItemTypes() {
+        ArrayList<String> types = new ArrayList<String>();
+        types.add("Food");
+        types.add("Water");
+        types.add("Hygiene Products");
+        return types;
+    }
 }
