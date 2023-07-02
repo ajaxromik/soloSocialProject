559b90f548a1f3f52579e09a2a8543b3606f2b24
Ajax <thelegohexbug@gmail.com>
2023-07-01 22:26:54 -0400

    removed unused imports

--- src/FoodPantry.java
+++ src/FoodPantry.java
@@ -1,14 +1,10 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Logger;
-
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-
 import java.util.logging.Level;
 
 /**
